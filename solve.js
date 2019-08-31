acbf1f413923ad2c64fc4ff8495ef500dc12f49f
github username <github email address>
2019-08-30 19:00:57 -0500

    factorialize

--- solve.js
+++ solve.js
@@ -26,3 +26,16 @@ function reversedString(str){
     return final;
   }
   console.log(reverseString("hello"))
+
+
+  //factorial the product of an integer and all the integers below it.
+  function factorialize(num){
+    var result = 1; //accumulate all the numbers. not 0 because anything times 0 is 0
+    for(var i = 1; i<=num; i++){
+      //result = result * i
+      result *= i
+    }
+    return result;
+  }
+  
+  factorialize(6)
